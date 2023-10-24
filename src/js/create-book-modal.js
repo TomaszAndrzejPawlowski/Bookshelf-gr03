dae4d25307fd9ad77b0ba84172798ef5c4629ef1
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-10-24 13:47:34 +0200

    dd
    dd

--- src/js/create-book-modal.js
+++ src/js/create-book-modal.js
@@ -4,18 +4,22 @@ export function createBookCard(bookDetails) {
 
   bookCard.innerHTML = `
     <div class="modal-card-container">
-    <div class="popup">
-    <button class="modal-close-button">
-    <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M21 7L7 21M7 7L21 21" stroke="#111111" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-</svg></button>
-    <img class="popup-img" src="${bookDetails.book_image}" alt="${bookDetails.title}" />
-    <h2 class="popup-title">${bookDetails.title}</h2>
-    <p class="popup-author">Author: ${bookDetails.author}</p>
-    <p>Description: ${bookDetails.description}</p>
+        <div class="popup">
+            <button class="modal-close-button">
+            <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21 7L7 21M7 7L21 21" stroke="#111111" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg></button>
+            <div>
+                <img class="popup-img" src="${bookDetails.book_image}" alt="${bookDetails.title}" />
+            </div>
+            <div class="popup-authorandtitle">
+                <h2 class="popup-title">${bookDetails.title}</h2>
+                <p class="popup-author">Author: ${bookDetails.author}</p>
+            </div>
 
-    
-    <button class="add-to-cart-button" data-book-id="${bookDetails._id}">Add to Cart</button>
+    <div class="modal-button-position">
+        <button class="add-to-cart-button" data-book-id="${bookDetails._id}">add to shopping list</button>
+    </div>
     </div>
     </div>
   `;
