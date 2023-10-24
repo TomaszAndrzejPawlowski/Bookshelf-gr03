c02386a4b92da5073d42d6bb6a813dd9d07fcfce
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-10-24 14:31:13 +0200

    style
    style modal tablet

--- src/js/create-book-modal.js
+++ src/js/create-book-modal.js
@@ -9,17 +9,18 @@ export function createBookCard(bookDetails) {
             <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M21 7L7 21M7 7L21 21" stroke="#111111" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
             </svg></button>
-            <div>
-                <img class="popup-img" src="${bookDetails.book_image}" alt="${bookDetails.title}" />
-            </div>
+            <img class="popup-img" src="${bookDetails.book_image}" alt="${bookDetails.title}" />
+            
             <div class="popup-authorandtitle">
                 <h2 class="popup-title">${bookDetails.title}</h2>
                 <p class="popup-author">Author: ${bookDetails.author}</p>
-            </div>
-
-    <div class="modal-button-position">
+                <p class="popup-desc"> ${bookDetails.description}</p>
+                <div class="modal-button-position">
         <button class="add-to-cart-button" data-book-id="${bookDetails._id}">add to shopping list</button>
     </div>
+            </div>
+
+    
     </div>
     </div>
   `;
