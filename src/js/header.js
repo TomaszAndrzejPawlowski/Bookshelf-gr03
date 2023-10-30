b4c9226fcd97fbbcb758a6bbdc4b62749ee5b8b0
JacekWoloszczak <jacek.woloszczak@gmail.com>
2023-10-30 18:57:58 +0100

    modal

--- src/js/header.js
+++ src/js/header.js
@@ -20,9 +20,9 @@ const modalBtn = document.querySelector('.modal_btn');
 const hamburgerOpenBtn = document.querySelector('.hamburger_open');
 const hamburgerCloseBtn = document.querySelector('.hamburger_close');
 const modalMobile = document.querySelector('.modal_mobile');
-const modalSignupMobile = document.querySelector('.modal_signup-mobile');
+
 const buttonMobileModal = document.querySelector('.btn_log-out-mobile');
-const buttonMobileModalClose = document.querySelector('.modal_singup_close-mobile');
+
 const iconMail = document.querySelector('.modal_input-icon-mail');
 const iconPass = document.querySelector('.modal_input-icon-pass');
 const iconWhiteMail = document.querySelector('.modal_input-icon-mail-white');
@@ -141,6 +141,8 @@ openModalBtn.addEventListener('click', () => {
 closeModalBtn.addEventListener('click', () => {
   modal.classList.toggle('is_hidden');
   backdrop.style.display = 'none';
+  hamburgerCloseBtn.style.display = 'none';
+  hamburgerOpenBtn.style.display = 'block';
 });
 
 hamburgerOpenBtn.addEventListener('click', () => {
@@ -156,17 +158,9 @@ hamburgerCloseBtn.addEventListener('click', () => {
 });
 
 buttonMobileModal.addEventListener('click', () => {
-  modalSignupMobile.style.display = 'block';
+  modal.classList.toggle('is_hidden');
   modalMobile.style.display = 'none';
   backdrop.style.display = 'block';
-  modalSignupMobile.style.position = 'fixed';
-});
-
-buttonMobileModalClose.addEventListener('click', () => {
-  modalSignupMobile.style.display = 'none';
-  hamburgerCloseBtn.style.display = 'none';
-  hamburgerOpenBtn.style.display = 'block';
-  backdrop.style.display = 'none';
 });
 
 modalBtn.addEventListener('click', e => {
